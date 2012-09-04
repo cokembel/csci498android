c71752984a6ddfd84988002a3393d21d2fe0f5ca
Cole Kembel <cokembel@mines.edu>
2012-09-04 11:05:19 -0600

    Types radioGroup adjusted to set member variable types.

--- LunchList/src/csci498/cokembel/lunchlist/LunchList.java
+++ LunchList/src/csci498/cokembel/lunchlist/LunchList.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 
 
 public class LunchList extends Activity {
@@ -31,6 +32,19 @@ public class LunchList extends Activity {
 			r.setName(name.getText().toString());
 			r.setAddress(address.getText().toString());
 			
+			RadioGroup types = (RadioGroup) findViewById(R.id.types);
+			
+			switch (types.getCheckedRadioButtonId()){
+				case R.id.sit_down:
+					r.setType("sit_down");
+					break;
+				case R.id.take_out:
+					r.setType("take_out");
+					break;
+				case R.id.delivery:
+					r.setType("delivery");
+					break;
+			}	
 		}
 	};
 
