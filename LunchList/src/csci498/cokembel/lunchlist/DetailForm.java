3c0c5d7c4a442974bae439b209752a3479e7fa21
Cole Kembel <cokembel@mines.edu>
2012-10-02 20:05:10 -0600

    Stored widget information in state Bundle in onSaveInstanceState in DetailForm.

--- LunchList/src/csci498/cokembel/lunchlist/DetailForm.java
+++ LunchList/src/csci498/cokembel/lunchlist/DetailForm.java
@@ -75,6 +75,11 @@ public class DetailForm extends Activity {
 	@Override
 	public void onSaveInstanceState(Bundle state) {
 		super.onSaveInstanceState(state);
+		
+		state.putString("name", name.getText().toString());
+		state.putString("address", address.getText().toString());
+		state.putString("notes", notes.getText().toString());
+		state.putInt("type", types.getCheckedRadioButtonId());
 	}
 
 	private View.OnClickListener onSave = new View.OnClickListener() {
