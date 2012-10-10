5d9690698957f1379997983a3a61329af27945cf
Cole Kembel <cokembel@mines.edu>
2012-10-10 00:19:02 -0600

    Implemented onHandleIntent in FeedService to use a RSSReader to get RSSFeed.

--- LunchList/src/csci498/cokembel/lunchlist/FeedService.java
+++ LunchList/src/csci498/cokembel/lunchlist/FeedService.java
@@ -1,18 +1,31 @@
 package csci498.cokembel.lunchlist;
 
+import org.mcsoxford.rss.RSSReader;
+
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
+
 
 public class FeedService extends IntentService {
+	
+	public static final String EXTRA_URL = "csci498.cokembel.lunchlist.EXTRA_URL";
 	
 	public FeedService() {
 		super("FeedService");
 	}
 
 	@Override
-	protected void onHandleIntent(Intent intent) {
-		// TODO Auto-generated method stub
-
+	protected void onHandleIntent(Intent i) {
+		RSSReader reader = new RSSReader();
+		
+		try {
+			RSSFeed result = reader.load(i.getStringExtra(EXTRA_URL));
+		} catch (Exception e) {
+			Log.e("LunchList", "Exception parsing feed", e);
+		}
 	}
 
 }
