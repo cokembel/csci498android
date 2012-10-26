c50b398145e9a0cd660dddbfdc41effb9d161ff6
Cole Kembel <cokembel@mines.edu>
2012-10-26 17:21:28 -0600

    Implemented onCreate in RestaurantMap

--- LunchList/src/csci498/cokembel/lunchlist/RestaurantMap.java
+++ LunchList/src/csci498/cokembel/lunchlist/RestaurantMap.java
@@ -1,12 +1,22 @@
 package csci498.cokembel.lunchlist;
 
+import android.R;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import com.google.android.maps.MapActivity;
 
 public class RestaurantMap extends MapActivity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.map);
+	}
 
 	@Override
 	protected boolean isRouteDisplayed() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
