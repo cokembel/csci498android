54a91f77f783b70595255e253719785676263cba
Cole Kembel <cokembel@mines.edu>
2012-10-26 17:51:27 -0600

    Added string data members for Intent extras.

--- LunchList/src/csci498/cokembel/lunchlist/RestaurantMap.java
+++ LunchList/src/csci498/cokembel/lunchlist/RestaurantMap.java
@@ -9,6 +9,10 @@ import com.google.android.maps.MapActivity;
 
 public class RestaurantMap extends MapActivity {
 	
+	public static final String EXTRA_LATITUDE = "csci498.cokembel.lunchlist.EXTRA_LATITUDE";
+	public static final String EXTRA_LONGITUDE = "csci498.cokembel.lunchlist.EXTRA_LONGITUDE";
+	public static final String EXTRA_NAME = "csci498.cokembel.lunchlist.EXTRA_NAME";
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
