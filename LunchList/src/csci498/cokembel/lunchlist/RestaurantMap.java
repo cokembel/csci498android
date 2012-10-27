b6202eabbf5f9c1013e28f59f1ae1cb36b496bee
Cole Kembel <cokembel@mines.edu>
2012-10-26 18:19:33 -0600

    Used MapView variable to adjust zoom and map center upon initialization in RestaurantMap.

--- LunchList/src/csci498/cokembel/lunchlist/RestaurantMap.java
+++ LunchList/src/csci498/cokembel/lunchlist/RestaurantMap.java
@@ -1,17 +1,21 @@
 package csci498.cokembel.lunchlist;
 
-import android.R;
+import csci498.cokembel.lunshlist.R;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 
 public class RestaurantMap extends MapActivity {
 	
 	public static final String EXTRA_LATITUDE = "csci498.cokembel.lunchlist.EXTRA_LATITUDE";
 	public static final String EXTRA_LONGITUDE = "csci498.cokembel.lunchlist.EXTRA_LONGITUDE";
 	public static final String EXTRA_NAME = "csci498.cokembel.lunchlist.EXTRA_NAME";
+	
+	private MapView map = null;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,15 @@ public class RestaurantMap extends MapActivity {
 		
 		double lat = getIntent().getDoubleExtra(EXTRA_LATITUDE, 0);
 		double lon = getIntent().getDoubleExtra(EXTRA_LONGITUDE, 0);
+		
+		map = (MapView)findViewById(R.id.map);
+		map.getController().setZoom(17);
+		
+		GeoPoint status = new GeoPoint((int)(lat*1000000.0),(int)(lon*1000000.0));
+		
+		map.getController().setCenter(status);
+		map.setBuiltInZoomControls(true);
+		
 	}
 
 	@Override
