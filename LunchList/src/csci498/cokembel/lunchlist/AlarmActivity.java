62f997955b05dae078538e882473b16fc8852bdd
Cole Kembel <cokembel@mines.edu>
2012-10-30 00:17:53 -0600

    Created AlarmActivity class file and added implemenation.

---
+++ LunchList/src/csci498/cokembel/lunchlist/AlarmActivity.java
@@ -0,0 +1,15 @@
+package csci498.cokembel.lunchlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+import csci498.cokembel.lunshlist.R;
+
+public class AlarmActivity extends Activity{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.alarm);
+	}
+
+}
