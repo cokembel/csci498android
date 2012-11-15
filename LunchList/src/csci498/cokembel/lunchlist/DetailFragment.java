8a11f27e59ce13481f1d69ad5b3919cf6d7c5ff0
Cole Kembel <cokembel@mines.edu>
2012-11-15 02:41:05 -0700

    In onOptionsItemSelected in both LunchFragment and DetailFragment, added initialization of HelpPage activity upon selecting help item.

--- LunchList/src/csci498/cokembel/lunchlist/DetailFragment.java
+++ LunchList/src/csci498/cokembel/lunchlist/DetailFragment.java
@@ -183,6 +183,8 @@ public class DetailFragment extends Fragment {
 			
 			startActivity(i);
 			return true;
+		} else if (item.getItemId() == R.id.help) {
+			   startActivity(new Intent(getActivity(), HelpPage.class));
 		}
 		
 		return super.onOptionsItemSelected(item);
